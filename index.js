da01426148cf8c5d07101e37880f4f1474caeb9f
Elian Doran <elinnum@gmail.com>
2018-11-01 18:39:54 +0200

    Set up Express Handlebars integration

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 const app = express();
 const port = 1337;
+const handlebars = require("handlebars");
+const resume = require("./cv.json");
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-    res.send("Resume goes here.");
+    res.render("resume", resume);
 });
 
 app.listen(port, () => {
